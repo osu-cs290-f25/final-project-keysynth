ec5ea0cf465d7818a787f7f44316df62c81c0df3
Jeremy <jeremyhmcelhone@gmail.com>
2025-11-21 21:41:29 -0800

    add LP filter

--- index.js
+++ index.js
@@ -3,24 +3,30 @@ let startButton = document.getElementById("start-button");
 let volumeSlider = document.getElementById("volume-slider");
 let attackSlider = document.getElementById("attack-slider");
 let releaseSlider = document.getElementById("release-slider");
+let filterSlider = document.getElementById("filter-slider");
 let waveformDropdown = document.getElementById("waveform-dropdown");
 let keys = Array.from(document.getElementById("keys").children);
 
 const notes = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25];
 
+// AudioContext nodes
 let ctx = new AudioContext();
+let filter = ctx.createBiquadFilter();
 let compressor = ctx.createDynamicsCompressor();
+filter.connect(compressor);
 compressor.connect(ctx.destination);
 
 // Synth Settings
-let volume = 1;
+let volume = 0.9;
 let attack = 0;
 let release = 0.1;
 let waveform = "sine";
+let filterFrequency = 10000;
 volumeSlider.value = volume;
 attackSlider.value = attack;
 releaseSlider.value = release;
-waveformDropdown.value = "sine";
+filterSlider.value = filterFrequency;
+waveformDropdown.value = waveform;
 
 
 function playNote(frequency) {
@@ -29,9 +35,11 @@ function playNote(frequency) {
 
     osc.type = waveform;
     osc.frequency.value = frequency;
+    filter.frequency.value = filterFrequency;
+    filter.Q.value = -1;
 
     // Connections
-    gain.connect(compressor);
+    gain.connect(filter);
     osc.connect(gain);
     osc.start();
 
@@ -57,6 +65,11 @@ attackSlider.oninput = function() {
 
 releaseSlider.oninput = function() {
     release = releaseSlider.value;
+}
+
+filterSlider.oninput = function() {
+    console.log(filterFrequency);
+    filterFrequency = Math.pow(10000, filterSlider.value);
 }
 
 waveformDropdown.oninput = function() {
